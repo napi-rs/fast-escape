6061ab57b3a1bcbb0c962b292d97d2b936a6cb6a
github-action-benchmark <github@users.noreply.github.com>
2020-11-27 17:20:18 +0000

    add Benchmark (benchmarkjs) benchmark result for 7bafc18cac0190044061ac8461b608448d841125

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1603092059630,
+  "lastUpdate": 1606497618396,
   "repoUrl": "https://github.com/napi-rs/fast-escape",
   "entries": {
     "Benchmark": [
@@ -80,6 +80,86 @@ window.BENCHMARK_DATA = {
             "range": "±0.88%",
             "unit": "ops/sec",
             "extra": "88 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lynweklm@gmail.com",
+            "name": "LongYinan",
+            "username": "Brooooooklyn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7bafc18cac0190044061ac8461b608448d841125",
+          "message": "Merge pull request #38 from napi-rs/napi-1-pre\n\nNapi 1 pre",
+          "timestamp": "2020-11-28T01:17:19+08:00",
+          "tree_id": "a4fba37df610d6db94e09969a2fe5980604dc8f2",
+          "url": "https://github.com/napi-rs/fast-escape/commit/7bafc18cac0190044061ac8461b608448d841125"
+        },
+        "date": 1606497617760,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "napi @ Large input",
+            "value": 563,
+            "range": "±1.08%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          },
+          {
+            "name": "napi#buff @ Large input",
+            "value": 684,
+            "range": "±0.76%",
+            "unit": "ops/sec",
+            "extra": "88 samples"
+          },
+          {
+            "name": "napi#asyncBuff @ Large input",
+            "value": 617,
+            "range": "±0.96%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          },
+          {
+            "name": "javascript @ Large input",
+            "value": 440,
+            "range": "±0.98%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          },
+          {
+            "name": "napi @ Small input",
+            "value": 1492838,
+            "range": "±1.01%",
+            "unit": "ops/sec",
+            "extra": "91 samples"
+          },
+          {
+            "name": "napi#buff @ Small input",
+            "value": 1651702,
+            "range": "±1.19%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
+          },
+          {
+            "name": "napi#asyncBuff @ Small input",
+            "value": 27255,
+            "range": "±1.80%",
+            "unit": "ops/sec",
+            "extra": "78 samples"
+          },
+          {
+            "name": "javascript @ Small input",
+            "value": 1307238,
+            "range": "±0.80%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
           }
         ]
       }
